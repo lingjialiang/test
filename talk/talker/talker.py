f7c13abf73eb694bb003dc3f51306bc679ce0a86
lingjialiang <40281666+lingjialiang@users.noreply.github.com>
2018-08-07 13:05:51 +0900

    Update talker.py

--- talk/talker/talker.py
+++ talk/talker/talker.py
@@ -1,20 +1,27 @@
-#!/usr/bin/env python
-
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import rospy
 from std_msgs.msg import String
+import kddiapi as kd
+import os,sys
 
 def talker():
-	pub = rospy.Publisher('telemetry', String, queue_size=10)
-	rospy.init_node('talker', anonymous=True)
-	rate = rospy.Rate(10)
-	while not rospy.is_shutdown():
-		hello_str = "its rainny %s" % rospy.get_time()
-		rospy.loginfo(hello_str)
-		pub.publish(hello_str)
-		rate.sleep()
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(0.001) # 10hz
+    while not rospy.is_shutdown():            
+        kddi=kd.k()
+        title='[日時,湿度,温度,明るさ,紫外線,気圧,音量,不快指数,熱中症危険度,CO2濃度]'
+        for i in range(len(kddi)):
+            del kddi[i][0:2]
+            rospy.loginfo(title)
+            pub.publish(title)
+            rospy.loginfo(kddi[i])
+            pub.publish(kddi[i])
+        rate.sleep()  
 
 if __name__ == '__main__':
-	try:
-		talker()
-	except rospy.ROSInterruptException:
-		pass
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
