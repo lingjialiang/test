1a8e0c6458cf5fe1718fd5b0e6f69f9dbaa2ab36
lingjialiang <40281666+lingjialiang@users.noreply.github.com>
2018-08-07 13:27:45 +0900

    Add files via upload

---
+++ talk/talker/kddiapi.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+#coding:UTF-8
+import requests, json
+from datetime import datetime,timedelta
+import csv
+
+def k():
+    now=datetime.now()
+    before=now+timedelta(hours=-1)
+    url = "http://logger.m2m-cloud-std.kddi.ne.jp/v1.0/data/regular"
+    method = "POST"
+    obj = {
+        "terminals" :
+           [ {
+        "terminal_id" : "410",
+        "no" : [2,3,4,5,6,7,8,9,14]
+            }],
+       # "from" : "{0:%Y%m%d%H%M%S}".format(before),
+       #  "to" : "{0:%Y%m%d%H%M%S}".format(now)
+        "from" : "20180801101000",
+        "to" : "20180801105000"
+    }
+    json_data = json.dumps(obj).encode("utf-8")
+    headers = {"Host":"logger.m2m-cloud-std.kddi.ne.jp",
+              "Authorization":"Bearer W2DEAhjNvxlvwVlM-sIYmsyHYsfrCHgPi" ,
+    "Content-Type" : "application/json",
+    "Accept":"text/csv"}
+
+    r = requests.post(url=url, data=json_data, headers=headers)
+    print(r)
+    data=[]
+    kddi=[]
+    data=r.text
+    w=data.splitlines()
+    for i in range(len(w)):
+        kddi.append(w[i].encode('utf8').split(','))
+    del kddi[0]
+    return kddi
+    
+    
+    
+if __name__ == '__main__':
+    kddi=k()
+    for i in range(len(kddi)):
+        del kddi[i][0:2]
+        print("[日時, 湿度, 温度, 明るさ, 紫外線, 気圧, 音量, 不快指数, 熱中症危険度, CO2濃度]")
+        print(kddi[i])
